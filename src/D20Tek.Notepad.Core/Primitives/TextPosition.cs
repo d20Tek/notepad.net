727eaf01835e4179d840dbfe49bc629d31d7c3df
Pedro Silva <DarthPedro@gmail.com>
2026-02-13 15:52:46 -0800

    Added Notepad.Core and Notepad.Core.UnitTets projects. Implemented TextPosition as the first core type. Ensured central package management updated the required test dependencies.

---
+++ src/D20Tek.Notepad.Core/Primitives/TextPosition.cs
@@ -0,0 +1,16 @@
+﻿namespace D20Tek.Notepad.Core.Primitives;
+
+public readonly record struct TextPosition(int Line, int Column) : IComparable<TextPosition>
+{
+    public int CompareTo(TextPosition other)
+    {
+        var lineCompare = Line.CompareTo(other.Line);
+        return lineCompare != 0 ? lineCompare : Column.CompareTo(other.Column);
+    }
+
+    public override string ToString() => $"({Line}, {Column})";
+
+    public static TextPosition Min(TextPosition a, TextPosition b) => a.CompareTo(b) <= 0 ? a : b;
+
+    public static TextPosition Max(TextPosition a, TextPosition b) => a.CompareTo(b) >= 0 ? a : b;
+}
---
+++ tests/D20Tek.Notepad.Core.UnitTests/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace D20Tek.Notepad.Core.UnitTests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
